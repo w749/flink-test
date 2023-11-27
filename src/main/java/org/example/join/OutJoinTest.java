c6330c4d88d6b35809840bb09ca3a70fd42c7ff8
wangxukun <wangxukun@haohandata.com>
2023-11-27 16:36:05 +0800

    commit

--- src/main/java/org/example/join/OutJoinTest.java
+++ src/main/java/org/example/join/OutJoinTest.java
@@ -3,7 +3,9 @@ package org.example.join;
 import lombok.SneakyThrows;
 import org.apache.flink.api.common.functions.CoGroupFunction;
 import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.common.functions.RichCoGroupFunction;
 import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
@@ -21,10 +23,14 @@ public class OutJoinTest implements Base {
                         .where((KeySelector<Order, String>) Order::getItem)
                         .equalTo((KeySelector<Rate, String>) Rate::getItem)
                         .window(TumblingEventTimeWindows.of(Time.seconds(10)))
-                        .apply(new CoGroupFunction<Order, Rate, Result>() {
+                        .apply(new RichCoGroupFunction<Order, Rate, Result>() {
+
                             @Override
                             public void coGroup(Iterable<Order> first, Iterable<Rate> second, Collector<Result> out) throws Exception {
                                 for (Order order : first) {
+                                    if (!second.iterator().hasNext()) {
+                                        out.collect(new Result(order.getOrderTime(), order.getPrice(), order.getItem()));
+                                    }
                                     for (Rate rate : second) {
                                         out.collect(new Result(order.getOrderTime(), order.getPrice() * rate.getRate(), order.getItem()));
                                     }
