7f27fbab0c3a9c53989083aa37b8c1074c609b00
wangxukun <wangxukun@haohandata.com>
2023-11-27 16:29:41 +0800

    commit

--- src/main/java/org/example/join/OutJoinTest.java
+++ src/main/java/org/example/join/OutJoinTest.java
@@ -12,6 +12,8 @@ import org.example.entity.Order;
 import org.example.entity.Rate;
 import org.example.entity.Result;
 
+import java.util.Iterator;
+
 public class OutJoinTest implements Base {
     @SneakyThrows
     public static void main(String[] args) {
@@ -19,9 +21,13 @@ public class OutJoinTest implements Base {
                         .where((KeySelector<Order, String>) Order::getItem)
                         .equalTo((KeySelector<Rate, String>) Rate::getItem)
                         .window(TumblingEventTimeWindows.of(Time.seconds(10)))
-                        .apply((CoGroupFunction<Order, Rate, Result>) (first, second, out) -> first.forEach(order ->
-                                second.forEach(rate ->
-                                        out.collect(new Result(order.getOrderTime(), order.getPrice() * rate.getRate(), order.getItem()))))).print();
+                        .apply((CoGroupFunction<Order, Rate, Result>) (first, second, out) -> {
+                            for (Order order : first) {
+                                for (Rate rate : second) {
+                                    out.collect(new Result(order.getOrderTime(), order.getPrice() * rate.getRate(), order.getItem()));
+                                }
+                            }
+                        }).print();
 
         environment.execute();
     }
